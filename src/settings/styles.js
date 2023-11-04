8d32d9ad2ae318a9dbea5108555b5330fe57add9
Kowalchick <kkowalczyk91@gmail.com>
2023-11-04 11:35:08 +0100

    Add styles general file with variables to export

---
+++ src/settings/styles.js
@@ -0,0 +1,12 @@
+export const colors = {
+    pink: '#EE3A68',
+    pinkie:'rgba(255, 99, 132, 0.5)',
+    lightPink: '#FFF8F9',
+    mainBlack: '#1E0108',
+    gray: '#C9CBCF',
+    blue: '#3BA0EB',
+    white: '#ffffff',
+    violet: 'rgba(161, 0, 255, 0.5)',
+  };
+
+  export const fontBase = "'Montserrat', sans-serif";
