679ce286f026f0192ab220c8dfb6f04922d56463
Kowalchick <kkowalczyk91@gmail.com>
2023-11-08 08:38:49 +0100

    Update the app state to keep and display the user answers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import Welcome from "./components/Welcome/Welcome";
 import Questions from "./components/Questions/Questions";
@@ -7,33 +7,36 @@ import Results from "./components/Results/Results";
 import Login from "./components/Login/Login";
 
 const App = () => {
-    const [step, setStep] = useState(1);
-    const [activeQuestion, setActiveQuestion] = useState(0);
-    const [answers, setAnswers] = useState([]);
+  const [step, setStep] = useState(1);
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [answers, setAnswers] = useState([]);
 
-    const surveyStartHandler = () => {
-        setStep(2);
-    }
+  const surveyStartHandler = () => {
+    setStep(2);
+  };
 
   return (
     <div className="App">
-        {step === 1 && <Welcome onSurveyStart={surveyStartHandler}/>}
-        {step === 2 && <Login onSetStep={setStep} />}
-        {step === 3 && <Questions
-            data={surveyData.data[activeQuestion]}
-            onAnswerUpdate={setAnswers}
-            numberOfQuestions={surveyData.data.length}
-            activeQuestion={activeQuestion}
-            onSetActiveQuestion={setActiveQuestion}
-            onSetStep={setStep}
-        />}
-        {step === 4 && <Results
-            results={answers}
-            dataAnswer={surveyData.dataAnswer}
-            dataAnswer2={surveyData.dataAnswer2}
-            />}
+      {step === 1 && <Welcome onSurveyStart={surveyStartHandler} />}
+      {step === 2 && <Login onSetStep={setStep} />}
+      {step === 3 && (
+        <Questions
+          data={surveyData.data[activeQuestion]}
+          onAnswerUpdate={setAnswers}
+          numberOfQuestions={surveyData.data.length}
+          activeQuestion={activeQuestion}
+          onSetActiveQuestion={setActiveQuestion}
+          onSetStep={setStep}
+        />
+      )}
+      {step === 4 && (
+        <Results
+          results={answers}
+          dataAnswer={surveyData.dataAnswer}
+          dataAnswer2={surveyData.dataAnswer2}
+        />
+      )}
     </div>
   );
-}
+};
 export default App;
-
--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,56 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import { styled } from '@mui/material/styles';
-import {Chart} from "../Chart/ChartComponent";
-import {colors} from "../../settings/styles";
+import React, { useState, useEffect } from "react";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { styled } from "@mui/material/styles";
+import { Chart } from "../Chart/ChartComponent";
+import { colors } from "../../settings/styles";
 
 const StyledFavoriteIcon = styled(FavoriteIcon)({
-    color: colors.pink,
-    paddingLeft: "10px",
-  });
+  color: colors.pink,
+  paddingLeft: "10px",
+});
 
 function useMediaQuery(query) {
-    const [matches, setMatches] = useState(window.matchMedia(query).matches);
+  const [matches, setMatches] = useState(window.matchMedia(query).matches);
 
-    useEffect(() => {
-        const media = window.matchMedia(query);
-        const listener = () => setMatches(media.matches);
-        
-        media.addEventListener('change', listener);
-        return () => media.removeEventListener('change', listener);
-    }, [query]);
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    const listener = () => setMatches(media.matches);
 
-    return matches;
+    media.addEventListener("change", listener);
+    return () => media.removeEventListener("change", listener);
+  }, [query]);
+
+  return matches;
 }
 
-const Results = ({ onAnswersCheck, dataAnswer, dataAnswer2 }) => {
+const Results = ({ results, dataAnswer, dataAnswer2 }) => {
+  const isMobile = useMediaQuery("(max-width: 582px)");
 
-    const isMobile = useMediaQuery('(max-width: 582px)');
+  const userAnswersData = results.map(answer => answer.value);
+  const teamAnswersData = dataAnswer2[0].data[0].values; 
 
-    const chartData = isMobile ? dataAnswer2[0] : dataAnswer[0];
+  const chartData = isMobile ? dataAnswer2[0] : dataAnswer[0];
 
-    return (
-        <div className="card">
-            <div className="card-content">
-                <div className="content">
-                    <h1 className="thanks">
-                        Thank you for your time
-                        <StyledFavoriteIcon />
-                    </h1>
-                    <div className='content-box'>
-                        <h2 className="thanks-subtitle">Find your results below</h2>
-                        <Chart
-                            labels={chartData.labels}
-                            data1={chartData.data[0].values}
-                            data2={chartData.data[1].values}
-                            isMobile={isMobile}
-                        />
-                    </div>
-                    <button className="btn" onClick={onAnswersCheck}>Send results to my email</button>
-                </div>
-            </div>
+  return (
+    <div className="card">
+      <div className="card-content">
+        <div className="content">
+          <h1 className="thanks">
+            Thank you for your time
+            <StyledFavoriteIcon />
+          </h1>
+          <div className="content-box">
+            <h2 className="thanks-subtitle">Find your results below</h2>
+            <Chart
+              labels={chartData.labels}
+              userAnswersData={userAnswersData} 
+              teamAnswersData={teamAnswersData}
+              isMobile={isMobile}
+            />
+          </div>
+          <button className="btn">
+            Send results to my email
+          </button>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Results;
